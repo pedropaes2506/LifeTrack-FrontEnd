7213611c2baf12be2ef4313c5858c46335a1f7be
Gustavo D. <gugadename@gmail.com>
2025-11-17 13:34:24 -0300

    telas perfil, alterar senha e calculadora

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import LandingPage from './components/LandingPage';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import Cadastro from './components/Cadastro';
+import AlterarSenha from './components/AlterarSenha';
+import MeuPerfil from './components/MeuPerfil';
+import Calculadora from './components/Calculadora';
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/alterar-senha" element={<AlterarSenha />} />
+        <Route path="/meu-perfil" element={<MeuPerfil />} />
+        <Route path="/calculadora" element={<Calculadora />} />
       </Routes>
     </Router>
   );
